323845e249d288a9ee6df4ec9a9406860863bbcc
Arsenij72 <arsenij.alejnikov@stud.techin.lt>
2025-01-07 12:57:58 +0200

    upd

--- uzduotis/uzduotis/01_05.Story/src/main/java/Story.java
+++ uzduotis/uzduotis/01_05.Story/src/main/java/Story.java
@@ -5,8 +5,10 @@ public class Story {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Write your program here
-
+        System.out.println("What is the main character called? ");
+        String name = scanner.nextLine();
+        System.out.println("What is the main character called? ");
+        String job = scanner.nextLine();
+        System.out.println("Here is the story: Once upon a time there was " + name "asd");
     }
 }
