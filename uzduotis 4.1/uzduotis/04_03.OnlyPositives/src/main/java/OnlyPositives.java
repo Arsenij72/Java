214d154aa3854007b775f1f4b2343e3eab52f760
Arsenij72 <arsenij.alejnikov@stud.techin.lt>
2025-01-09 06:46:03 +0200

    4.1

--- uzduotis 4.1/uzduotis/04_03.OnlyPositives/src/main/java/OnlyPositives.java
+++ uzduotis 4.1/uzduotis/04_03.OnlyPositives/src/main/java/OnlyPositives.java
@@ -1,10 +1,20 @@
-
 import java.util.Scanner;
 
 public class OnlyPositives {
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            System.out.println("Give a number:");
+            int input = scan.nextInt();
+            if (input > 0) {
+                System.out.println(input *= 2);
+            }
+            if (input < 0) {
+                System.out.println("Unsuitable number");
+            } else if (input == 0) {
 
+                break;
+            }
+        }
     }
 }
