091e0ae68dd67621a4c05bdfeb3b6a3d8d2a6ab1
Arsenij72 <arsenij.alejnikov@stud.techin.lt>
2025-01-13 14:06:09 +0200

    6.1

---
+++ uzduotis 6.1/uzduotis/06_01.Swap/src/main/java/Swap.java
@@ -0,0 +1,39 @@
+
+import java.util.Scanner;
+
+public class Swap {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[5];
+        int swap;
+        array[0] = 1;
+        array[1] = 3;
+        array[2] = 5;
+        array[3] = 7;
+        array[4] = 9;
+
+        int index = 0;
+        while (index < array.length) {
+            System.out.println(array[index]);
+            index++;
+        }
+        System.out.println("");
+
+        System.out.println("Give two indices to swap:");
+        int firstNumber = Integer.parseInt(scanner.nextLine());
+        int secondNumber = Integer.parseInt(scanner.nextLine());
+
+        swap = array[firstNumber];
+        array[firstNumber] = array[secondNumber];
+        array[secondNumber] = swap;
+
+        System.out.println("");
+        index = 0;
+        while (index < array.length) {
+            System.out.println(array[index]);
+            index++;
+        }
+    }
+
+}
